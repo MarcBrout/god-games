f594a4ae850412e7193f42d1775cd41cbc6e88c5
Nico <33263148+NicoPlattner@users.noreply.github.com>
2018-09-13 09:34:33 +0200

    camera movement and zoom

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+    /*
+    Vector3 p1position;
+    Vector3 p2position;
+    */
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+}
