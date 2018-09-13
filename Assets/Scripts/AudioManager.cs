77f433bdf9406e533f72f3640e784d8f1e19636b
Demis <demis_terborg@outlook.com>
2018-09-13 17:32:46 +0200

    add public methods. add comments

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,16 +1,18 @@
 ﻿/*
- *Audio class used to change between songs.
+ * Audio class used to change between songs.
  * By: Demis Terborg
- * 
- * Make a reference to this class in your script, then drag and drop
- * the AudioManager GameObject onto the reference
- * to make use of the public methods.
  * 
  * HOWTO: Add AudioClips
  * 1. Name the AudioClip by adding an enum under 'public enum AudioTrack {}'.
  * 2. Declare an AudioClip using the same naming scheme (line 43).
- * 3. Add the AudioClip to the switch case in function 'AudioClip GetAudioClip(AudioTrack track) {}' (line 125).
+ * 3. Add the AudioClip to the switch case in function 'AudioClip GetAudioClip(AudioTrack track) {}' (line 171).
  * 4. Attach an audio file to the AudioClip by dragging and dropping the file in the inspector view.
+ * 
+ * HOWTO: Access public methods
+ * 1. Make a public reference to this class in your script.
+ * 2. Drag and drop the AudioManager GameObject onto the reference in the 'Inspector view'.
+ * 3. Access the member functions using the member-access operator ( . ) between the object variable name and the member name.
+ * 
  */
 
 using System.Collections;
@@ -48,6 +50,7 @@ namespace GodsGame
 
         // ************************ PUBLIC METHODS ************************ //
 
+        // TODO: add extra param to make FadeOut optional. Get rid of redundant code.
         // Plays AudioClip depending on which clip has been selected.
         // Also checks which AudioSource should be used, to prevent two AudioSources from playing simultaneously.
         public void playAudio(AudioTrack track)
@@ -66,6 +69,45 @@ namespace GodsGame
             }
         }
 
+        // TODO: add extra param to make FadeOut optional. Get rid of redundant code.
+        public void stopAudio()
+        {
+            if (audioSource1Playing)
+            {
+                StartCoroutine(FadeOut(audioSource1, timeToFade));
+            }
+            else
+            {
+                StartCoroutine(FadeOut(audioSource2, timeToFade));
+            }
+        } 
+
+        // TODO: add extra param to make fadeOut optional. Get rid of redundant code.
+        public void pauseAudio()
+        {
+            if (audioSource1Playing)
+            {
+                audioSource1.Pause();
+            }
+            else
+            {
+                audioSource2.Pause();
+            }
+        }
+
+        // TODO: add extra param to make fadeIn optional. Get rid of redundant code.
+        public void unPauseAudio()
+        {
+            if (audioSource1Playing)
+            {
+                audioSource1.UnPause();
+            }
+            else
+            {
+                audioSource2.UnPause();
+            }
+        }
+
         // TODO: Implement coroutine that triggers playAudio when currentAudio.volume < 0.5f //
         // Checks which AudioSource is currently playing. Fades out the currently played audio,
         // While fading in the chosen AudioClip.
@@ -73,7 +115,7 @@ namespace GodsGame
         {
             if (audioSource1Playing)
             {
-                stopAudio(ref audioSource1);
+                StartCoroutine(FadeOut(audioSource1, timeToFade));
                 playAudio(ref audioSource2, GetAudioClip(track));
 
                 // Sets audioSource2 as primary AudioSource
@@ -81,7 +123,7 @@ namespace GodsGame
             }
             else
             {
-                stopAudio(ref audioSource2);
+                StartCoroutine(FadeOut(audioSource2, timeToFade));
                 playAudio(ref audioSource1, GetAudioClip(track));
 
                 // Sets audioSource1 as primary AudioSource
@@ -111,12 +153,18 @@ namespace GodsGame
 
             // Set AudioManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
             DontDestroyOnLoad(gameObject);
-        }
 
-        private void Start()
-        {
             // Sets audioSource1 as primary AudioSource
             audioSource1Playing = true;
+        }
+
+        // TODO: add extra param to make FadeIn optional
+        // Used by function ChangeAudio
+        private void playAudio(ref AudioSource audioSource, AudioClip audioClip)
+        {
+            audioSource.clip = audioClip;
+            audioSource.Play();
+            StartCoroutine(FadeIn(audioSource, timeToFade));
         }
 
         // Returns the AudioClip that corresponds to the enum value.
@@ -129,32 +177,6 @@ namespace GodsGame
                 case AudioTrack.PauseMenu: return Clip_02;
                 default: return null;
             }
-        }
-
-        // TODO: add extra param to make FadeIn optional
-        private void playAudio(ref AudioSource audioSource, AudioClip audioClip)
-        {
-            audioSource.clip = audioClip;
-            audioSource.Play();
-            StartCoroutine(FadeIn(audioSource, timeToFade));
-        }
-
-        // TODO: add extra param to make FadeOut optional
-        private void stopAudio(ref AudioSource audioSource)
-        {
-            StartCoroutine(FadeOut(audioSource, timeToFade));
-        }
-
-        // TODO: add extra param to make fadeOut optional
-        private void pauseAudio(ref AudioSource audioSource)
-        {
-            audioSource.Pause();
-        }
-
-        // TODO: add extra param to make fadeIn optional
-        private void unPauseAudio(ref AudioSource audioSource)
-        {
-            audioSource.UnPause();
         }
 
         // Coroutine to fade-in the AudioSource by lowering the volume.
@@ -196,7 +218,6 @@ namespace GodsGame
 
         // ************************ TESTING PURPOSES ************************ //
 
-        /*
         public void test()
         {
             playAudio(AudioTrack.Fight);
@@ -223,6 +244,5 @@ namespace GodsGame
             yield return new WaitForSeconds(6);
             changeAudio(AudioTrack.PauseMenu);
         }
-        */
     }
 }
