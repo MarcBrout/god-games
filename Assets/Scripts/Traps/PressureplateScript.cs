9826dd1959f914d7a2eb895e2a03aa0081db3dc3
Tim <timg040497@gmail.com>
2018-09-18 10:37:59 +0200

    pressurplate and spike

---
+++ Assets/Scripts/Traps/PressureplateScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressureplateScript : MonoBehaviour {
+
+    public Animator pressurPlateAnimator;
+    public SpikeTrapScript spikeTrapScript;
+    public bool isEnable;
+    private TrapManagerScript trapManager;
+    public int trapmanagerId;
+
+	// Use this for initialization
+	void Start () {
+        isEnable = true;
+    }
+
+    public void Init(TrapManagerScript trapManager, int trapmanagerId){
+        this.trapManager = trapManager;
+        this.trapmanagerId = trapmanagerId;
+    }
+
+    public void plateTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player" ||  col.gameObject.tag == "Boss")
+        {
+            if (isEnable) {
+                pressurPlateAnimator.SetBool("playerEntered", true);
+                spikeTrapScript.ActivateTrap();
+                disableTrap();
+            }
+        }
+    }
+
+    public void plateTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "Boss")
+        {
+            pressurPlateAnimator.SetBool("playerEntered", false);
+            spikeTrapScript.DeactivateTrap();
+
+            if (trapManager == null)
+            {
+                enableTrap();
+            }
+        }
+    }
+
+    void disableTrap()
+    {
+        isEnable = false;
+        pressurPlateAnimator.SetBool("isEnable", false);
+
+        if (trapManager != null) {
+            trapManager.disableTrap(trapmanagerId);
+        }
+    }
+
+    public void enableTrap()
+    {
+        isEnable = true;
+        pressurPlateAnimator.SetBool("isEnable", true);
+        spikeTrapScript.ShowEnableColor();
+
+    }
+}
---
+++ Assets/Scripts/Traps/TrapManagerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapManagerScript : MonoBehaviour {
+
+    public List<PressureplateScript> pressureplates;
+    private List<bool> pressureplateState = new List<bool>();
+
+    // Use this for initialization
+    void Start () {
+
+        for (int i = 0; i < pressureplates.Count; i++)
+        {
+            pressureplates[i].Init(this, i+1);
+            pressureplateState.Add(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void disableTrap(int id) {
+        
+        pressureplateState[id-1] = false;
+        
+        if (areAllTrapsDisabled()) {
+            enableAllTrapsExpectOne(id);
+        }
+    }
+
+    bool areAllTrapsDisabled() {
+
+        foreach(bool state in pressureplateState)
+        {
+            if (state) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void enableAllTrapsExpectOne(int id) {
+        for (int i = 0; i < pressureplateState.Count; i++)
+        {
+            if (i != id-1)
+            {
+                pressureplateState[i] = true;
+                pressureplates[i].enableTrap();
+            }
+        }
+    }
+}
