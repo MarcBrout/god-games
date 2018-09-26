7b23ee9a45e8ec102d5c6a5abee3c73b2b4ef813
Demis <demis_terborg@outlook.com>
2018-09-26 13:12:22 +0200

    add pressureplate scripts to namespace GodsGame

--- Assets/Scripts/Traps/PressureplateScript.cs
+++ Assets/Scripts/Traps/PressureplateScript.cs
@@ -3,82 +3,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PressureplateScript : MonoBehaviour {
+namespace GodsGame
+{
+    public class PressureplateScript : MonoBehaviour
+    {
 
-    public Animator pressurPlateAnimator;
-    public GameObject trapScriptGameObject;
-    private ITrapInterface trapScript;
-    private bool isEnable;
-    private TrapManagerScript trapManager;
-    private int trapmanagerId;
-    private AudioSource[] sounds;
+        public Animator pressurPlateAnimator;
+        public GameObject trapScriptGameObject;
+        private ITrapInterface trapScript;
+        private bool isEnable;
+        private TrapManagerScript trapManager;
+        private int trapmanagerId;
+        private AudioSource[] sounds;
 
-	// Use this for initialization
-	void Start () {
+        // Use this for initialization
+        void Start()
+        {
 
-        trapScript = trapScriptGameObject.GetComponent<ITrapInterface>();
-        if (trapScript == null) {
-            throw new Exception("Missing Trapinterface on the Trap Script Gameobject");
+            trapScript = trapScriptGameObject.GetComponent<ITrapInterface>();
+            if (trapScript == null)
+            {
+                throw new Exception("Missing Trapinterface on the Trap Script Gameobject");
 
+            }
+
+            isEnable = true;
+            sounds = GetComponents<AudioSource>();
+            Debug.Log(sounds.Length);
         }
 
-        isEnable = true;
-        sounds = GetComponents<AudioSource>();
-        Debug.Log(sounds.Length);
-    }
-
-    public void Init(TrapManagerScript trapManager, int trapmanagerId){
-        this.trapManager = trapManager;
-        this.trapmanagerId = trapmanagerId;
-    }
-
-    public void plateTriggerEnter(Collider col)
-    {
-        if (col.gameObject.tag == "Player" ||  col.gameObject.tag == "Boss")
+        public void Init(TrapManagerScript trapManager, int trapmanagerId)
         {
-            if (isEnable) {
-                sounds[1].Play();
-                pressurPlateAnimator.SetBool("playerEntered", true);
-                trapScript.ActivateTrap();
-                disableTrap();
-            }
+            this.trapManager = trapManager;
+            this.trapmanagerId = trapmanagerId;
+        }
+
+        public void plateTriggerEnter(Collider col)
+        {
+            if (col.gameObject.tag == "Player" || col.gameObject.tag == "Boss")
+            {
+                if (isEnable)
+                {
+                    sounds[1].Play();
+                    pressurPlateAnimator.SetBool("playerEntered", true);
+                    trapScript.ActivateTrap();
+                    disableTrap();
+                }
                 else
-            {
-                sounds[0].Play();
+                {
+                    sounds[0].Play();
+                }
             }
         }
-    }
 
-    public void plateTriggerExit(Collider col)
-    {
-        if (col.gameObject.tag == "Player" || col.gameObject.tag == "Boss")
+        public void plateTriggerExit(Collider col)
         {
-            pressurPlateAnimator.SetBool("playerEntered", false);
-            trapScript.DeactivateTrap();
-
-            if (trapManager == null)
+            if (col.gameObject.tag == "Player" || col.gameObject.tag == "Boss")
             {
-                enableTrap();
+                pressurPlateAnimator.SetBool("playerEntered", false);
+                trapScript.DeactivateTrap();
+
+                if (trapManager == null)
+                {
+                    enableTrap();
+                }
             }
         }
-    }
 
-    void disableTrap()
-    {
-        isEnable = false;
-        pressurPlateAnimator.SetBool("isEnable", false);
+        void disableTrap()
+        {
+            isEnable = false;
+            pressurPlateAnimator.SetBool("isEnable", false);
 
-        if (trapManager != null) {
-            trapManager.disableTrap(trapmanagerId);
+            if (trapManager != null)
+            {
+                trapManager.disableTrap(trapmanagerId);
+            }
         }
-    }
 
-    public void enableTrap()
-    {
-        sounds[2].Play();
-        isEnable = true;
-        pressurPlateAnimator.SetBool("isEnable", true);
-        trapScript.ShowEnableColor();
+        public void enableTrap()
+        {
+            sounds[2].Play();
+            isEnable = true;
+            pressurPlateAnimator.SetBool("isEnable", true);
+            trapScript.ShowEnableColor();
 
+        }
     }
 }
--- Assets/Scripts/Traps/SpikeTrapScript.cs
+++ Assets/Scripts/Traps/SpikeTrapScript.cs
@@ -2,40 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpikeTrapScript : MonoBehaviour, ITrapInterface {
-
-    public Animator spikesAnimator;
-    public GameObject border;
-    private Color borderColor;
-    public Color disabledTrapColor = Color.gray;
-
-    // Use this for initialization
-    void Start () {
-        borderColor = border.GetComponent<Renderer>().material.GetColor("_Color");
-    }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
-
-    public void ActivateTrap() {
-        spikesAnimator.SetBool("isActivated", true);
-
-    }
-
-    public void DeactivateTrap()
+namespace GodsGame
+{
+    public class SpikeTrapScript : MonoBehaviour, ITrapInterface
     {
-        spikesAnimator.SetBool("isActivated", false);
+
+        public Animator spikesAnimator;
+        public GameObject border;
+        private Color borderColor;
+        public Color disabledTrapColor = Color.gray;
+
+        // Use this for initialization
+        void Start()
+        {
+            borderColor = border.GetComponent<Renderer>().material.GetColor("_Color");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void ActivateTrap()
+        {
+            spikesAnimator.SetBool("isActivated", true);
+
+        }
+
+        public void DeactivateTrap()
+        {
+            spikesAnimator.SetBool("isActivated", false);
+
+        }
+
+        public void ShowDisabledColor()
+        {
+            border.GetComponent<Renderer>().material.SetColor("_Color", disabledTrapColor);
+        }
+
+        public void ShowEnableColor()
+        {
+            border.GetComponent<Renderer>().material.SetColor("_Color", borderColor);
+        }
 
     }
-
-    public void ShowDisabledColor() {
-        border.GetComponent<Renderer>().material.SetColor("_Color", disabledTrapColor);
-    }
-
-    public void ShowEnableColor() {
-        border.GetComponent<Renderer>().material.SetColor("_Color", borderColor);
-    }
-
 }
