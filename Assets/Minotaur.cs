05e96ae7be08c9e2db60e2c0701eb584c0f7f921
Marc Brout <marc.brout@epitech.eu>
2018-09-26 20:56:55 +0200

    [added] audio manager

---
+++ Assets/Minotaur.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GodsGame {
+    public class Minotaur : MonoBehaviour {
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        public void onTakeDamage(Damager damager, Damageable damageable)
+        {
+            Debug.Log("I GOT HIT ARG");
+        }
+    }
+}
