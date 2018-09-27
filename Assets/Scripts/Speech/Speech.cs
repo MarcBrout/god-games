de89b4b1fba4b7b73a30177e8cb7f8e694b748d5
Demis <demis_terborg@outlook.com>
2018-09-27 13:06:32 +0200

    add SpeechEventManager logic

---
+++ Assets/Scripts/Speech/Speech.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speech
+{
+    public string[] playerBeforeFight =
+    {
+        "Gordian Knot!",
+        "Cretans always lie",
+        "Lord have mercy",
+        "Apple of Discord"
+    };
+
+    public string[] playerDuringFight =
+    {
+        "We have won",
+        "God from the machine",
+        "I have found [it]",
+        "I came, I saw, I conquered",
+        "Death is no different than life",
+        "You won't get me"
+    };
+
+}
