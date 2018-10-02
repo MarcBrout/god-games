031b749fc73a51b62e96c20b059dd11e75afb1eb
Tim <timg040497@gmail.com>
2018-10-02 14:10:50 +0200

    3d pressurplate and trap model implementation

--- Assets/Scripts/Traps/SpikeTrapScript.cs
+++ Assets/Scripts/Traps/SpikeTrapScript.cs
@@ -7,15 +7,22 @@ namespace GodsGame
     public class SpikeTrapScript : MonoBehaviour, ITrapInterface
     {
 
-        public Animator spikesAnimator;
-        public GameObject border;
-        private Color borderColor;
-        public Color disabledTrapColor = Color.gray;
+        public Material enabledTrapMaterial;
+        public Material disabledTrapMaterial;
+        public GameObject ringParent;
+        private List<GameObject> trapRings;
+        private Animator _spikesAnimator;
 
         // Use this for initialization
         void Start()
         {
-            borderColor = border.GetComponent<Renderer>().material.GetColor("_Color");
+            _spikesAnimator = GetComponent<Animator>();
+            trapRings = new List<GameObject>();
+
+            foreach (Transform child in ringParent.transform) {
+                trapRings.Add(child.gameObject);
+            }
+            ShowEnableColor();
         }
 
         // Update is called once per frame
@@ -26,24 +33,29 @@ namespace GodsGame
 
         public void ActivateTrap()
         {
-            spikesAnimator.SetBool("isActivated", true);
+            _spikesAnimator.SetBool("isActivated", true);
 
         }
 
         public void DeactivateTrap()
         {
-            spikesAnimator.SetBool("isActivated", false);
+            _spikesAnimator.SetBool("isActivated", false);
 
         }
 
         public void ShowDisabledColor()
         {
-            border.GetComponent<Renderer>().material.SetColor("_Color", disabledTrapColor);
+            foreach (GameObject item in trapRings) {
+                item.GetComponent<Renderer>().material = disabledTrapMaterial;
+            }
         }
 
         public void ShowEnableColor()
         {
-            border.GetComponent<Renderer>().material.SetColor("_Color", borderColor);
+            foreach (GameObject item in trapRings)
+            {
+                item.GetComponent<Renderer>().material = enabledTrapMaterial;
+            }
         }
 
     }
