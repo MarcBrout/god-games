03023ef5054eee4fddc2677e3d52cc1ff92efa64
Demis <demis_terborg@outlook.com>
2018-10-17 18:03:17 +0200

    add code for linking audio with menu

---
+++ Assets/Scripts/Audio/MixLevels.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixLevels : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+
+    public void SetSfxLevel(float sfxLvl)
+    {
+        masterMixer.SetFloat("sfxVol", sfxLvl);
+    }
+
+    public void SetMusicLevel(float musicLvl)
+    {
+        masterMixer.SetFloat("musicVol", musicLvl);
+    }
+
+    public void SetMasterLevel(float masterLvl)
+    {
+        masterMixer.SetFloat("masterVol", masterLvl);
+    }
+}
---
+++ Assets/Scripts/Audio/PauseManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace GodsGame
+{
+    public class PauseManager : MonoBehaviour
+    {
+        public AudioMixerSnapshot paused;
+        public AudioMixerSnapshot unpaused;
+
+        private Canvas canvas;
+
+        void Start()
+        {
+            canvas = GetComponent<Canvas>();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                canvas.enabled = !canvas.enabled;
+                PauseMusic();
+            }
+        }
+
+        public void PauseMusic()
+        {
+            Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        }
+
+        void LowPass()
+        {
+            if (Time.timeScale == 0)
+            {
+                paused.TransitionTo(.01f);
+            }
+
+            else
+            {
+                unpaused.TransitionTo(.01f);
+            }
+        }
+    }
+}
