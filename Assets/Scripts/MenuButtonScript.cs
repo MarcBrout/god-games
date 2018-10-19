6ddb4a8c69f0547d480b4cc213da53a4377928cd
Tim <timg040497@gmail.com>
2018-10-19 11:47:36 +0200

    Menu Stuff

---
+++ Assets/Scripts/MenuButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GodsGame
+{
+    public class MenuButtonScript : MonoBehaviour
+    {
+
+        public void PlayerHoverSound()
+        {
+            AudioManager.Instance.PlaySfx("button_hover","menu");
+        }
+
+        public void PlayerClickSound()
+        {
+            AudioManager.Instance.PlaySfx("button_click", "menu");
+
+        }
+    }
+}
