e87047783c06989d286771dd34d38a0253bdac25
Marc Brout <marc.brout@epitech.eu>
2018-11-05 15:10:49 +0100

    pillars detects rope cuts

--- Assets/ColumnEvents.cs
+++ Assets/ColumnEvents.cs
@@ -7,12 +7,26 @@ namespace GodsGame
     public class ColumnEvents : MonoBehaviour
     {
         public Animator _animator;
-
+        public GameObject[] _ropes;
 
         // Use this for initialization
-        void Start()
+        void Update()
         {
+            bool allDeactivated = true;
 
+            foreach (GameObject rope in _ropes)
+            {
+                if (rope.active)
+                {
+                    allDeactivated = false;
+                    break;
+                }
+            }
+            
+            if (allDeactivated)
+            {
+                _animator.SetTrigger("Fall");
+            }
         }
 
         public void OnActivate(Activable activable, Trigger trigger)
