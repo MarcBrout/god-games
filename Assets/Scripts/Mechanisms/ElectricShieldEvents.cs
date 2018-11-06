07bb9ac605bc8ef88b0a625064bbf438751da1b0
Marc Brout <marc.brout@epitech.eu>
2018-11-06 16:44:21 +0100

    added sound on electric shields

--- Assets/Scripts/Mechanisms/ElectricShieldEvents.cs
+++ Assets/Scripts/Mechanisms/ElectricShieldEvents.cs
@@ -8,23 +8,34 @@ namespace GodsGame
     {
         ParticleSystem _particleSystem;
         Collider _collider;
+        private AudioSource _audioSource;
 
         // Use this for initialization
         void Start()
         {
             _particleSystem = GetComponent<ParticleSystem>();
             _collider = GetComponent<Collider>();
+            _audioSource = GetComponent<AudioSource>();
         }
 
         public void Disable(Activable activable, Trigger trigger)
         {
             _particleSystem.Stop();
             _collider.enabled = false;
+            AudioManager.Instance.PlaySfx3D("shield_down", "items_common", ref _audioSource);
         }
 
         public void Enable(Activable activable, Trigger trigger)
         {
+            AudioManager.Instance.PlaySfx3D("shield_up", "items_common", ref _audioSource);
+
             _particleSystem.Play();
+            StartCoroutine(enableAfter());
+        }
+
+        public IEnumerator enableAfter()
+        {
+            yield return new WaitForSeconds(1000);
             _collider.enabled = true;
         }
     }
