bec22cf2696446abd53d8697d5c018e024215f7e
Marc Brout <marc.brout@epitech.eu>
2018-11-25 14:53:16 +0100

    Shockwave explosion effect

---
+++ Assets/ShockWaveObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GodsGame
+{
+    [RequireComponent(typeof(Collider))]
+    public class ShockWaveObject : MonoBehaviour
+    {
+        public float _SpawnInterval = 0.1f;
+        public float _LifeTime = 0.3f;
+        private Collider _Collider;
+
+        private void Start()
+        {
+            _Collider = GetComponent<Collider>();
+        }
+
+        // Use this for initialization
+        void Awake()
+        {
+            StartCoroutine(SpawnNext());
+            StartCoroutine(Die());
+        }
+
+        private void onTriggerEnter(Collider collider)
+        {
+            Debug.Log("COLLIISSSIOON");
+            if (collider.gameObject.layer == 9)
+            {
+                StopCoroutine(SpawnNext());
+            }
+        }
+
+        private void onTriggerStay(Collider collider)
+        {
+            Debug.Log("COLLIISSSIOON");
+            if (collider.gameObject.layer == 9)
+            {
+                StopCoroutine(SpawnNext());
+                _Collider.enabled = false;
+            }
+        }
+
+        private IEnumerator SpawnNext()
+        {
+            yield return new WaitForSeconds(_SpawnInterval);
+            GameObject next = Instantiate(gameObject, transform.position + new Vector3(1, 0, 0), transform.rotation);
+        }
+
+        private IEnumerator Die()
+        {
+            yield return new WaitForSeconds(_LifeTime);
+            Destroy(gameObject);
+        }
+    }
+}
