09c9183251304416f62c1e1be4a742c6c490b8ca
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2018-11-26 10:39:23 +0100

    Merging

--- Assets/Scripts/LaunchAnimationOnTrigger.cs
+++ Assets/Scripts/LaunchAnimationOnTrigger.cs
@@ -5,17 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class LaunchAnimationOnTrigger : MonoBehaviour {
 
-    public int NumberTrigger = 0;
+    public int numberTrigger = 0;
+    public int sceneIndex = 0;
+
     private int _triggerCount = 0;
 
-	public void AddTriggerCount()
+    public void AddTriggerCount()
     {
         _triggerCount++;
         Debug.Log(_triggerCount);
-        if (_triggerCount >= NumberTrigger)
+        if (_triggerCount >= numberTrigger)
         {
             _triggerCount = 0;
-            SceneManager.LoadScene(4, LoadSceneMode.Single);
+            SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
         }
     }
 
