55961dec551d21039d479a4ffb49dc9c8b2882e9
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2018-11-28 22:40:16 +0100

    Adding indication on level introduction. Waiting for real sprite

---
+++ Assets/CanvasEvent.cs
@@ -0,0 +1,26 @@
+﻿using GodsGame;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasEvent : MonoBehaviour {
+
+    public Canvas canvas;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void OnActivate(Activable activable, Trigger trigger)
+    {
+        canvas.GetComponent<Canvas>().enabled = true;
+    }
+
+    public void OnDeactivate(Activable activable, Trigger trigger)
+    {
+        Debug.Log("LEL");
+        canvas.GetComponent<Canvas>().enabled = false;
+    }
+}
