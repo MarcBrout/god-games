2ce8d7d39884fff50a8beb820648523f603eacfb
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2018-12-09 16:32:24 +0100

    Adding Cutscene Minotaur

--- Assets/Scripts/AI/Bosses/Minotaur/Minotaur.cs
+++ Assets/Scripts/AI/Bosses/Minotaur/Minotaur.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.Playables;
 
 namespace GodsGame {
     public class Minotaur : MonoBehaviour {
 
         public Animator animator;
-
+        public PlayableDirector timeline;
+                
         static private string[] BoohSounds = new string[] {
             "arena_crowd_booh",
             "arena_crowd_booh_and_cheer",
@@ -13,6 +15,12 @@ namespace GodsGame {
         public void OnTakeDamage(Damager damager, Damageable damageable)
         {
             Debug.Log("BOSS IS HIT !");
+            Debug.Log(damageable.CurrentHealth);
+            if (damageable.CurrentHealth == 5)
+            {
+                Debug.Log("Timeline PLAY !");
+                timeline.Play();
+            }
             CrowdManager.Instance.SetState(SpectatorBehaviour.STATES.PLAYER_HITTEN, 1000);
             AudioManager.Instance.PlaySfx(BoohSounds[Random.Range(0, BoohSounds.Length)], "arena_ambience");
             animator.SetInteger("life", damageable.CurrentHealth);
