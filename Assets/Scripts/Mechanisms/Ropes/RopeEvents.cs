fac3cfedd98576bdfc83e330ebdbf804f81e47a2
Marc Brout <marc.brout@epitech.eu>
2018-12-12 21:47:58 +0100

    [Add] Ropes Hit Effects

--- Assets/Scripts/Mechanisms/Ropes/RopeEvents.cs
+++ Assets/Scripts/Mechanisms/Ropes/RopeEvents.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 namespace GodsGame {
     public class RopeEvents : MonoBehaviour {
 
+        public ParticleSystem HitEffect;
         AudioSource _audioSource;
 
         // Use this for initialization
         void Start() {
             _audioSource = GetComponent<AudioSource>();
+            if (HitEffect == null)
+            {
+                Debug.LogWarning("HitEffect not set on " + gameObject.name);
+            }
         }
 
         public void OnDamage(Damager damager, Damageable damageable)
         {
             AudioManager.Instance.PlaySfx3D("hit_on_rope", "items_common", ref _audioSource);
+            if (HitEffect)
+            {
+                HitEffect.Play();
+            }
         }
 
         public void OnDie(Damager damager, Damageable damageable)
         {
             AudioManager.Instance.PlaySfx3D("rope_break", "items_common", ref _audioSource);
+            if (HitEffect)
+            {
+                HitEffect.Play();
+            }
         }
     }
 }
