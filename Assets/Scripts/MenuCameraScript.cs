d4a3c5ae0e71d4d45c5c04907d989d42de1efdf8
Tim <peipa@web.de>
2019-01-03 19:05:05 +0100

    Game Over and Level Complete menu with arena background

--- Assets/Scripts/MenuCameraScript.cs
+++ Assets/Scripts/MenuCameraScript.cs
@@ -6,8 +6,37 @@ public class MenuCameraScript : MonoBehaviour {
 
     // Update is called once per frame
     public float rotationSpeed = 1;
+    public float rotationCapLeft;
+    public float rotationCapRight;
+    public bool enableRotationCap;
+    private float _turnPointLeft;
+    private float _turnPointRight;
 
-	void Update () {
+    public void Start()
+    {
+        if (enableRotationCap) {
+            _turnPointLeft = transform.eulerAngles.y - rotationCapLeft;
+            _turnPointRight = transform.eulerAngles.y + rotationCapRight;
+
+        }
+    }
+
+    void Update () {
         transform.Rotate(Vector3.up * (Time.deltaTime * rotationSpeed));
+
+
+        if (enableRotationCap) {
+            if (rotationSpeed > 0)
+            {
+                if (transform.eulerAngles.y > _turnPointRight) {
+                    rotationSpeed = rotationSpeed * -1;
+                }
+            }
+            else {
+                if (transform.eulerAngles.y < _turnPointLeft) {
+                    rotationSpeed = rotationSpeed * -1;
+                }
+            }
+        }
     }
 }
