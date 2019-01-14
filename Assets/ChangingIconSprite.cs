a7029533b0f485a33d6e68596afad4f8d14bda0e
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2019-01-14 20:02:24 +0100

    Implement Zeus on the cinematic. Update cinematics subtitles by adding icon character

---
+++ Assets/ChangingIconSprite.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangingIconSprite : MonoBehaviour {
+
+    public Sprite newSprite;
+    public Image Image;
+
+    void OnEnable()
+    {
+        Image.sprite = newSprite;
+    }
+}
