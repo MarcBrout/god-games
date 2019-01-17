a39a1a7ad2cabbc6d0738c93257e1708fd9957b7
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2019-01-17 13:49:10 +0100

    Fix movement player on level 2

---
+++ Assets/DisableMovement.cs
@@ -0,0 +1,15 @@
+﻿using GodsGame;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableMovement : MonoBehaviour {
+
+    public PlayerBehaviour player;
+
+    private void OnEnable()
+    {
+        player.moveSpeed = 0;
+        player.turnSpeed = 0;
+    }
+}
---
+++ Assets/EnableMovement.cs
@@ -0,0 +1,15 @@
+﻿using GodsGame;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableMovement : MonoBehaviour {
+
+    public PlayerBehaviour player;
+
+    private void OnEnable()
+    {
+        player.moveSpeed = 8;
+        player.turnSpeed = 10;
+    }
+}
